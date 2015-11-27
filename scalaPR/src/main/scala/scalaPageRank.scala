2403694deb90304155fa27b1b144a30595e721a0
Ankit Gohil <gohilankit@.com>
2015-11-27 05:20:01 -0500

    Add reading from command line arguments

--- scalaPR/src/main/scala/scalaPageRank.scala
+++ scalaPR/src/main/scala/scalaPageRank.scala
@@ -6,20 +6,20 @@ import org.apache.spark.rdd.RDD
 
 object PageRank {
   def main(args: Array[String]) {
-    val logFile = "/home/craig/spark/README.md" // Should be some file on your system
-    val sc = new SparkContext("local", "PageRank", "$SPARK_HOME", List("target/scala-2.11/pagerank_2.11-1.0.jar"))
+   // val logFile =  // Should be some file on your system
+    val sc = new SparkContext()
     
 	// Load the edges as a graph
-	val graph = GraphLoader.edgeListFile(sc, "/home/ankit/Downloads/example_arcs")
+	val graph = GraphLoader.edgeListFile(sc, args(0))
 	// Run PageRank
 	val ranks = graph.pageRank(0.0001).vertices
 	// Join the ranks with the usernames
-	val pages = sc.textFile("/home/ankit/Downloads/example_index").map { line =>
-	  val fields = line.split("\\s")
-	  (fields(1).toLong, fields(0))
+	val pages = sc.textFile(args(1)).map { line =>
+	val fields = line.split("\\s")
+	(fields(1).toLong, fields(0))
 	}
 	val ranksByURLs = pages.join(ranks).map {
-	  case (id, (url, rank)) => (url, rank)
+	 case (id, (url, rank)) => (url, rank)
 	}
 	// Print the result
 	println(ranksByURLs.collect().mkString("\n"))
